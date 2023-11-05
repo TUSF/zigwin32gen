25c114c213685df7449d7e339f18cd6001fcb0df
Jeremia Dominguez <me@jeremia.dev>
2023-11-05 14:09:49 -0600

    Update to Zig 0.12.0-dev.1396+f6de3ec96
    Changes
    
    - Replaced the GitRepoStep with build.zig.zon to fetch the dependency.
    - std.builtin.Endian enum fields set to lower case.

--- build.zig
+++ build.zig
@@ -3,7 +3,6 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const Step = std.build.Step;
 const CrossTarget = std.zig.CrossTarget;
-const GitRepoStep = @import("GitRepoStep.zig");
 const patchstep = @import("patchstep.zig");
 
 pub fn build(b: *Builder) !void {
@@ -19,11 +18,7 @@ pub fn build(b: *Builder) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const win32json_repo = GitRepoStep.create(b, .{
-        .url = "https://github.com/marlersoft/win32json",
-        .branch = "21.0.3-preview",
-        .sha = "0337052317c881f541bd992c085347ff612725a8",
-    });
+    const win32json = b.dependency("win32json", .{});
 
     const run_pass1 = blk: {
         const pass1_exe = b.addExecutable(.{
@@ -34,8 +29,7 @@ pub fn build(b: *Builder) !void {
 
         const run_pass1 = b.addRunArtifact(pass1_exe);
         patchstep.patch(&run_pass1.step, runStepMake);
-        run_pass1.step.dependOn(&win32json_repo.step);
-        run_pass1.addArg(win32json_repo.getPath(&run_pass1.step));
+        run_pass1.addDirectoryArg(win32json.path(""));
 
         pass1_step.dependOn(&run_pass1.step);
         break :blk run_pass1;
@@ -50,7 +44,7 @@ pub fn build(b: *Builder) !void {
         const run = b.addRunArtifact(exe);
         patchstep.patch(&run.step, runStepMake);
         run.step.dependOn(&run_pass1.step);
-        run.addArg(win32json_repo.getPath(&run.step));
+        run.addDirectoryArg(win32json.path(""));
         break :blk &run.step;
     };
     gen_no_test_step.dependOn(run_genzig_step);
@@ -116,13 +110,13 @@ fn addExample(
 ) !void {
     const basename = concat(b, &.{ root, ".zig" });
     for (arches) |cross_arch_opt| {
-        const name = if (cross_arch_opt) |arch| concat(b, &.{ root, "-", arch}) else root;
+        const name = if (cross_arch_opt) |arch| concat(b, &.{ root, "-", arch }) else root;
 
-        const arch_os_abi = if (cross_arch_opt) |arch| concat(b, &.{ arch, "-windows"}) else "native";
+        const arch_os_abi = if (cross_arch_opt) |arch| concat(b, &.{ arch, "-windows" }) else "native";
         const target = std.zig.CrossTarget.parse(.{ .arch_os_abi = arch_os_abi }) catch unreachable;
         const exe = b.addExecutable(.{
             .name = name,
-            .root_source_file = .{ .path = b.pathJoin(&.{ "examples", basename}) },
+            .root_source_file = .{ .path = b.pathJoin(&.{ "examples", basename }) },
             .target = target,
             .optimize = optimize,
         });
@@ -132,11 +126,11 @@ fn addExample(
         examples_step.dependOn(&exe.step);
 
         const desc_suffix: []const u8 = if (cross_arch_opt) |_| "" else " for the native target";
-        const build_desc = b.fmt("Build {s}{s}", .{name, desc_suffix});
+        const build_desc = b.fmt("Build {s}{s}", .{ name, desc_suffix });
         b.step(concat(b, &.{ name, "-build" }), build_desc).dependOn(&exe.step);
 
         const run_cmd = b.addRunArtifact(exe);
-        const run_desc = b.fmt("Run {s}{s}", .{name, desc_suffix});
+        const run_desc = b.fmt("Run {s}{s}", .{ name, desc_suffix });
         b.step(name, run_desc).dependOn(&run_cmd.step);
 
         if (builtin.os.tag == .windows) {
